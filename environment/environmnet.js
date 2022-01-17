b24912d8fb5e578d51a0ae68ed4af6c0f4082bfa
ttomz <ttomz4291@gmail.com>
2022-01-17 22:26:26 +0200

    env

--- environment/environmnet.js
+++ environment/environmnet.js
@@ -2,5 +2,5 @@
 exports.env = {
     production: true,
     dbLocalURI: 'mongodb://127.0.0.1/grocery-list',
-    dbCloudURI: 'mongodb+srv://gl_admin:<PASSWORD>@cluster0.1jhmw.mongodb.net/test',
+    dbCloudURI: 'mongodb+srv://gl_admin:Tomas42910@cluster0.1jhmw.mongodb.net/test',
 };
